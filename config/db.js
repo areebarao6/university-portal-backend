4b807a8d682864e6fd7a1d7745672e5e22526c55
Areeba Javed <areejaved337@gmail.com>
2026-01-29 04:18:14 +0500

    Fix Mongoose 7 connection options

--- config/db.js
+++ config/db.js
@@ -1,17 +1,23 @@
 const mongoose = require("mongoose");
 
+// Load dotenv only in development
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
 const connectDB = async () => {
   try {
     if (!process.env.MONGO_URI) {
-      throw new Error("MONGO_URI is not defined in .env file");
+      throw new Error("MONGO_URI is not defined in environment variables");
     }
+
     await mongoose.connect(process.env.MONGO_URI);
     console.log("MongoDB Connected");
   } catch (error) {
     console.error("MongoDB Connection Error:", error.message);
-    console.error("Make sure MongoDB is running and MONGO_URI is correct in .env file");
     process.exit(1);
   }
 };
 
 module.exports = connectDB;
+
